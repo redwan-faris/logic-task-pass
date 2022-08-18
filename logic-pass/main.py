3cb1899c6280c0055a82bbb561bcf7852f0239aa
Redwan <redwanfarisa@gmail.com>
2022-08-18 10:39:53 +0300

    edit prime

--- logic-pass/main.py
+++ logic-pass/main.py
@@ -1,12 +1,24 @@
+import math
+
+from sqlalchemy import false;
+
 def remove_char(str,char):
     return str.replace(char,'')
 
-def find_prime_numbers(n):
-    primes = []
-    for number in range(n):
-        if(number % 2 ==0):
-            primes.append(number)
-    return primes
+
+#For some reasone i read it as even so this is the new sul:
+def find_primes(n):
+   primes = [];
+   for num in range(1, n  ): 
+        if num > 1:
+            for i in range(2, num):
+                if (num % i) == 0:
+                    break
+            else:
+                primes.append(num)
+   return primes
+print(find_primes(50))
+
 
 def count_repeated_characters(str,char):
     count = 0
@@ -14,6 +26,5 @@ def count_repeated_characters(str,char):
         if key == char:
             count +=1
     return count
-
-print(count_repeated_characters('reddeeee','d'))
+ 
         
